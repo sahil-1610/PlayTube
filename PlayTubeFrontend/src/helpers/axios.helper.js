226e792f69234a8fd417648ee06e41038b16c1e2
sahil-1610 <sahilkumartiwari.120372@marwadiuniversity.ac.in>
2024-09-25 01:25:45 +0530

    Refactor axios helper to use a hardcoded base URL

--- PlayTubeFrontend/src/helpers/axios.helper.js
+++ PlayTubeFrontend/src/helpers/axios.helper.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // import { loadProgressBar } from "axios-progress-bar";
 // import "axios-progress-bar/dist/nprogress.css";
 
-const baseURL = import.meta.env.VITE_BASE_URI;
+const baseURL = "https://playtube-wyh6.onrender.com/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
