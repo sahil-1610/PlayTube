7dc05dabda4556d25d04fdb140f5aa6460b41378
sahil-1610 <sahilkumartiwari.120372@marwadiuniversity.ac.in>
2024-09-25 01:23:29 +0530

    Refactor proxy configuration in vite.config.js

--- PlayTubeFrontend/vite.config.js
+++ PlayTubeFrontend/vite.config.js
@@ -1,19 +1,14 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load environment variables based on the mode
-  const env = loadEnv(mode, '.', '');  // '.' replaces process.cwd()
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_BASE_URI // Use the loaded environment variable
-        },
-      },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "https://playtube-wyh6.onrender.com/api/v1",
+    //  "/api": "http://localhost:9000/api/v1",
     },
-  };
+  },
 });
+
