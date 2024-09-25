65d89c185d280042501b95921cae98d1a3bebc78
sahil-1610 <sahilkumartiwari.120372@marwadiuniversity.ac.in>
2024-09-25 12:20:47 +0530

    Refactor axios helper to use environment variable for base URL

--- PlayTubeFrontend/src/helpers/axios.helper.js
+++ PlayTubeFrontend/src/helpers/axios.helper.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // import { loadProgressBar } from "axios-progress-bar";
 // import "axios-progress-bar/dist/nprogress.css";
 
-const baseURL = "https://playtube-wyh6.onrender.com/api/v1";
+const baseURL = import.meta.env.VITE_BASE_URI;
 
 export const axiosInstance = axios.create({
   baseURL,
